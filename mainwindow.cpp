acb744c25b4e78c7a2175e2f63b6fbe74b2f2c22
Mahmudul Hasan <hasan@uleth.ca>
2022-04-05 15:49:01 -0600

    Added winapi

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,8 +4,71 @@
 #include <QGridLayout>
 #include <QHBoxLayout>
 #include <QIcon>
+#include "windows.h"
 
 #include "mainwindow.h"
+
+using namespace cv;
+
+
+
+BITMAPINFOHEADER createBitmapHeader(int width, int height)
+{
+   BITMAPINFOHEADER  bi;
+
+     // create a bitmap
+     bi.biSize = sizeof(BITMAPINFOHEADER);
+     bi.biWidth = width;
+     bi.biHeight = -height;  //this is the line that makes it draw upside down or not
+     bi.biPlanes = 1;
+     bi.biBitCount = 32;
+     bi.biCompression = BI_RGB;
+     bi.biSizeImage = 0;
+     bi.biXPelsPerMeter = 0;
+     bi.biYPelsPerMeter = 0;
+     bi.biClrUsed = 0;
+     bi.biClrImportant = 0;
+
+     return bi;
+}
+
+Mat captureScreenMat(HWND hwnd)
+{
+     Mat src;
+
+     // get handles to a device context (DC)
+     HDC hwindowDC = GetDC(hwnd);
+     HDC hwindowCompatibleDC = CreateCompatibleDC(hwindowDC);
+     SetStretchBltMode(hwindowCompatibleDC, COLORONCOLOR);
+
+     // define scale, height and width
+     int screenx = GetSystemMetrics(SM_XVIRTUALSCREEN);
+     int screeny = GetSystemMetrics(SM_YVIRTUALSCREEN);
+     int width = GetSystemMetrics(SM_CXVIRTUALSCREEN);
+     int height = GetSystemMetrics(SM_CYVIRTUALSCREEN);
+
+     // create mat object
+     src.create(height, width, CV_8UC4);
+
+     // create a bitmap
+     HBITMAP hbwindow = CreateCompatibleBitmap(hwindowDC, width, height);
+     BITMAPINFOHEADER bi = createBitmapHeader(width, height);
+
+     // use the previously created device context with the bitmap
+     SelectObject(hwindowCompatibleDC, hbwindow);
+
+     // copy from the window device context to the bitmap device context
+     StretchBlt(hwindowCompatibleDC, 0, 0, width, height, hwindowDC, screenx, screeny, width, height, SRCCOPY);  //change SRCCOPY to NOTSRCCOPY for wacky colors !
+     GetDIBits(hwindowCompatibleDC, hbwindow, 0, height, src.data, (BITMAPINFO*)&bi, DIB_RGB_COLORS);            //copy from hwindowCompatibleDC to hbwindow
+
+     // avoid memory leak
+     DeleteObject(hbwindow);
+     DeleteDC(hwindowCompatibleDC);
+     ReleaseDC(hwnd, hwindowDC);
+
+     return src;
+ }
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent)
@@ -48,7 +111,6 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::onStart() {
-  using namespace cv;
 
   cv::VideoCapture camera(0);
   if (!camera.isOpened()) {
@@ -100,6 +162,20 @@ void MainWindow::onStart() {
 }
 
 void MainWindow::onStop() {
+
+  // capture image
+  HWND hwnd = GetDesktopWindow();
+  Mat src = captureScreenMat(hwnd);
+
+  // encode result
+  std::vector<uchar> buf;
+  cv::imencode(".png", src, buf);
+
+  // save img
+  cv::imwrite("d:/work/qt/test_cv/screenshot.png", src);
+
+  buf.clear();
+
 } 
 
 void MainWindow::on_inputPushButton_pressed()
--- test.pro
+++ test.pro
@@ -31,5 +31,7 @@ HEADERS += \
         mainwindow.h
 
 LIBS += -LD:\opencv_build\install\x64\vc17\lib -lopencv_core455d -lopencv_imgproc455d -lopencv_highgui455d -lopencv_imgcodecs455d -lopencv_videoio455d -lopencv_video455d -lopencv_calib3d455d -lopencv_photo455d -lopencv_features2d455d
+
+LIBS += -luser32 -ladvapi32 -lgdi32 -lcomdlg32
 INCLUDEPATH += D:\opencv_build\install\include
 DEPENDPATH += D:\opencv_build\install\include
